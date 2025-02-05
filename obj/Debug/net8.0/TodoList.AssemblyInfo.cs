761726cbd2bd3386ed8158d3f4195f4af87f6017
Rose Ngwenya <rose.ngwenya@deviare.africa>
2025-02-05 11:22:23 +0200

    5 February commit

--- obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3375b20472145ba4ba68201d9dfc325a3b3b22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe5b06c7223d79c5c3966d1f5a274f85eaed362")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
