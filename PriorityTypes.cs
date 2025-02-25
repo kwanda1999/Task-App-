22adbace7b0c61ba810d21095d65c5d945031ff6
Rose Ngwenya <rose.ngwenya@deviare.africa>
2025-02-25 14:37:46 +0200

    Task priority type model added

---
+++ PriorityTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoList
+{
+    public class PriorityTypes
+    {
+        public const string Urgent = "Urgent";
+
+        public const string NotUrgent = "Not Urgent";
+
+        public static List<string> GetAllPriorityTypes()
+        {
+            return new List<string>() { Urgent, NotUrgent };
+
+        }
+    }
+}
--- TodoItem.cs
+++ TodoItem.cs
@@ -4,8 +4,8 @@
         public string? Title { get; set; }
         public bool IsDone { get; set; }
         public string? Description { get; set; }
-
         public string? Category { get; set; }
+        public string? Priority { get; set; }
 
 
 }
